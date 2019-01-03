850099972a921fe9462fff170f4c068209a42f8c
Bakatparin <hs173251723@163.com>
2019-01-03 20:51:09 +0800

    vue-demo3

---
+++ app.js
@@ -0,0 +1,24 @@
+new Vue({
+	el:"#vue-app",
+	data:{
+		error:false,
+		success:false,
+		characters:["Mario","Luffy","Yoshi"],
+		user:{
+			name:"黄硕",
+			age:23
+		},
+		users:[
+			{name:"张三",age:23},
+			{name:"李四",age:28},
+			{name:"王五",age:30},
+		],
+
+	},
+	methods:{
+
+	},
+	computed:{
+
+	}
+});
